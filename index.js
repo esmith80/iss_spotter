acc685e6fd7c9773555ffb7fdaf2a75df6e54be6
Edward Smith <edward.m.smith80@gmail.com>
2020-08-28 05:01:56 +0000

    inital setup complete

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+/**
+ * Makes a single API request to retrieve the user's IP address.
+ * Input:
+ *   - A callback (to pass back an error or the IP string)
+ * Returns (via Callback):
+ *   - An error, if any (nullable)
+ *   - The IP address as a string (null if error). Example: "162.245.144.188"
+ */
+const request = require('request');
+
+const fetchMyIP = (callback) => { 
+  // use request to fetch IP address from JSON API
+  request('https://api.ipify.org?format=json', (error, response, body) => {
+    console.log(response);
+    console.log(error);
+    console.log(body);
+    console.log('hey');
+  });
+}
+
+module.exports = { fetchMyIP };
 
